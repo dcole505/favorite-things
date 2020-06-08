1883420b0c3f2f80541f192e61e541ab0bda03f5
David Cole <davidtcole@yahoo.com>
2020-06-08 16:16:23 -0700

    code to the scripts.js file to set the inputArray to hold the values of the form inputs. Used the push method and bracket notation to insert the 2nd, 1st, and 3rd items of the inputArray into the moddedArray. Also created jQuery to append each index of the inputArray into new list items in the unordered list with the id original.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
     const numberInput = parseInt($("input#number").val());
     const cityInput = $("input#city").val();
     const movieInput = $("input#movie").val();
-    const inputArray = [];
+    const inputArray = [animalInput, foodInput, colorInput, numberInput, cityInput, movieInput];
     const moddedArray = [];
+
+    moddedArray.push(inputArray[1], inputArray[0], inputArray[2]);
+
+    $("#original").append("<li>" + inputArray[0] + "</li>");
+    $("#original").append("<li>" + inputArray[1] + "</li>");
+    $("#original").append("<li>" + inputArray[2] + "</li>");
+    $("#original").append("<li>" + inputArray[3] + "</li>");
+    $("#original").append("<li>" + inputArray[4] + "</li>");
+    $("#original").append("<li>" + inputArray[5] + "</li>");
   });
 });
